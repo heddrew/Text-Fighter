77a8d25f8b04219adf18d4469ef4877bb4fbd70d
Drew <dev@heddrew.net>
2026-09-19 08:38:47 -0500

    Adjusted to new changes in GameClock.

--- src/tests/TestGameClock.java
+++ src/tests/TestGameClock.java
@@ -13,56 +13,51 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
-import time.GameClock;
+import com.hotmail.kalebmarc.textfighter.time.GameClock;
 
 public class TestGameClock {
 
 
-	
+
 	@Test
 	void testTimeConversion() {
-		GameClock newClock = new GameClock();
-		newClock.setStartTime(0);
-		newClock.setEndTime(60000);
-		long conversion = newClock.timeConversion();
-		assertEquals(conversion, (newClock.getIncreasedTime() + (newClock.getIncreasedTime() * newClock.getIncreasePercent())));
-		assertNotEquals(conversion, (newClock.getIncreasedTime() + ((newClock.getIncreasedTime() + 1) * newClock.getIncreasePercent())));
-		assertNotEquals(conversion, (newClock.getIncreasedTime() + (345346 * newClock.getIncreasePercent())));
+		GameClock.setStartTime(0);
+		GameClock.setEndTime(60000);
+		long conversion = GameClock.timeConversion();
+		assertEquals(conversion, (GameClock.getIncreasedTime() + (GameClock.getIncreasedTime() * GameClock.getIncreasePercent())));
+		assertNotEquals(conversion, (GameClock.getIncreasedTime() + ((GameClock.getIncreasedTime() + 1) * GameClock.getIncreasePercent())));
+		assertNotEquals(conversion, (GameClock.getIncreasedTime() + (345346 * GameClock.getIncreasePercent())));
 	}
 	
 	@Test
 	void testUpdateGameTime() {
 		LocalDateTime testTimeBase = LocalDateTime.of(LocalDate.of(2021, Month.DECEMBER, 2), LocalTime.of(5, 0, 0)); 
 		String testTimeFast;
-		GameClock newClock = new GameClock();
-		newClock.setBaseTime(testTimeBase);
-		newClock.setStartTime(0);
-		newClock.setEndTime(6000000);
-		newClock.updateGameTime();
-		testTimeFast = newClock.getFastTime().format(newClock.getDateTimeFormat());
-		assertFalse(testTimeFast.equals(newClock.getBaseTime().format(newClock.getDateTimeFormat())));
-		
+		GameClock.setBaseTime(testTimeBase);
+		GameClock.setStartTime(0);
+		GameClock.setEndTime(6000000);
+		GameClock.updateGameTime();
+		testTimeFast = GameClock.getFastTime().format(GameClock.getDateTimeFormat());
+		assertFalse(testTimeFast.equals(GameClock.getBaseTime().format(GameClock.getDateTimeFormat())));
 	}
 	
 
 	@BeforeClass
 	void testValues() {
-		GameClock newClock = new GameClock();
-		assertEquals(newClock.getStartTime(), 0);
-		assertEquals(newClock.getEndTime(), 0);
+		assertEquals(GameClock.getStartTime(), 0);
+		assertEquals(GameClock.getEndTime(), 0);
 	}
 	
 	@Test
 	void testDayRollOver() {
-		LocalDateTime testTimeBase = LocalDateTime.of(LocalDate.of(2021, Month.OCTOBER, 23), LocalTime.of(23, 59, 59)); 
-		GameClock newClock = new GameClock();
-		newClock.setBaseTime(testTimeBase);
-		newClock.splitDateTime(newClock.getBaseTime());
-		String baseDate = newClock.getGameDate();
-		newClock.setStartTime(0);
-		newClock.setEndTime(6000000);
-		newClock.updateGameTime();
-		String fastDate = newClock.getGameDate();
+		LocalDateTime testTimeBase = LocalDateTime.of(LocalDate.of(2021, Month.OCTOBER, 23), LocalTime.of(23, 59, 59));
+		GameClock.setBaseTime(testTimeBase);
+		GameClock.splitDateTime(GameClock.getBaseTime());
+		String baseDate = GameClock.getGameDate();
+		GameClock.setStartTime(0);
+		GameClock.setEndTime(6000000);
+		GameClock.updateGameTime();
+		String fastDate = GameClock.getGameDate();
 		assertFalse(fastDate.equalsIgnoreCase(baseDate));
 	}
 	
